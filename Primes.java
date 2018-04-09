277e5199f063e6f653d4108be9447f1f5790f29a
Eric L <eric@aol.com>
2018-04-09 10:24:19 -0500

    Updated primes

--- Primes.java
+++ Primes.java
@@ -3,22 +3,33 @@ package com.company;
 public class Primes {
     public static void main(String[] args) {
 
+        //Array to hold 1000 primes
         int[] primeNumbers = {};
+
+        //Number to be checked for Prime-ness
         int number = 1;
+
+        //Prime count and array position
         int primeCount = 0;
 
+        //Declare new primeNumbers array
         primeNumbers = new int[1000];
 
+        //Increases Prime Count and number if isPrime is true
         do {
             if (isPrime(number)){
                 primeNumbers[primeCount] = number;
                 primeCount++;
                 number++;
             }
+
+        //Increase only number if isPrime is false
             else {
                 number++;
             }
         }while(primeCount <= 999);
+
+        //Displays 1000th prime number
         System.out.println(primeNumbers[999]);
 
     }
